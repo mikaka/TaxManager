c45c48933ac424df29e77905d89fad1874b513fe
mikaka <petitane1@hotmail.com>
2025-06-26 15:41:22 +0200

    add new test Rename classes Better limitation of responsabilities

--- src/main/java/fr/pernisi/risf/taxManager/receipt/controller/ReceiptController.java
+++ src/main/java/fr/pernisi/risf/taxManager/receipt/controller/ReceiptController.java
@@ -3,7 +3,7 @@ package fr.pernisi.risf.taxmanager.receipt.controller;
 
 import fr.pernisi.risf.taxmanager.receipt.dto.ReceiptLineDto;
 import fr.pernisi.risf.taxmanager.receipt.model.Receipt;
-import fr.pernisi.risf.taxmanager.receipt.service.ParserService;
+import fr.pernisi.risf.taxmanager.receipt.service.TextsService;
 import fr.pernisi.risf.taxmanager.receipt.service.ReceiptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -19,16 +19,16 @@ import java.util.List;
 @Log4j2
 public class ReceiptController {
 
-    private final ParserService parserService;
+    private final TextsService textsService;
     private final ReceiptService receiptService;
 
     @PostMapping
     public String createReceipt(@RequestBody String input) {
         log.info("create a unit for "+input);
-        List<ReceiptLineDto> receiptLineList = parserService.parseInput(input);
+        List<ReceiptLineDto> receiptLineList = textsService.parseInput(input);
         Receipt receipt = receiptService.createReceipt(receiptLineList);
 
-        return parserService.formatReceipt(receipt);
+        return textsService.formatReceipt(receipt);
     }
 
 
